5a84b0af88fa4ed7aa4c26323fcfdbbf0644f7b5
Torsten Grote <t@grobox.de>
2014-07-21 23:12:30 +0200

    change server URLs

--- src/org/thoughtcrime/securesms/Release.java
+++ src/org/thoughtcrime/securesms/Release.java
@@ -2,8 +2,8 @@ package org.thoughtcrime.securesms;
 
 public class Release {
 
-  public static final String PUSH_URL = "http://sushiforeveryone.bluematt.me";
-  public static final String WS_URL   = "ws://sushiforeveryone.bluematt.me/v1/websocket/"; //TODO this is only local
+  public static final String PUSH_URL = "http://stallman.bas3.de:8080";
+  public static final String WS_URL   = "ws://stallman.bas3.de:8080/v1/websocket/"; //TODO this is only local
   public static final boolean DISABLE_GCM = true;
 
 //  public static final String PUSH_URL = "http://192.168.1.135:8080";
